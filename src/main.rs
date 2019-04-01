fbeabd733f82eff16dd869b92f6700043dce8e19
dissident <moreiras@informatik.uni-freiburg.de>
2019-04-01 21:23:27 +0200

    work in progress

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,13 @@ extern crate rulinalg;
 use tantivy::schema::*;
 use tantivy::Index;
 use tantivy::{DocId, DocSet, Postings};
+use tantivy::SegmentWriter;
+use tantivy::postings::serializer::*;
+use tantivy::postings::MultiFieldPostingsWriter;
+use tantivy::SegmentMeta;
+
+
+
 
 use std::collections::HashMap;
 
@@ -22,38 +29,16 @@ use rulinalg::matrix::{BaseMatrixMut, BaseMatrix, Matrix};
 use rulinalg::matrix::decomposition::FullPivLu;
 use std::str;
 
-#[macro_use] extern crate prettytable;
+extern crate prettytable;
 use prettytable::{Table, Row, Cell};
 
-fn simplify(a: f32) -> f32{
-    if a > 0.2 {
-        1.0
-    } else {
-        0.0
-    }
-}
-
-fn main() -> tantivy::Result<()> {
+fn main() {
     // Get result rows from tantivy.
-    let (index, field) = create_test_index()?;
-    let tantivy_result = get_tantivy_matrix(index, field)?;
-    let ak = calculate_lsa(tantivy_result);
-    Ok(())
-}
-fn calculate_lsa(mut tantivy_result: Vec<TantivyDocTermFreq>) -> tantivy::Result<Matrix<f32>> {
+    let (mut index, field) = create_test_index().unwrap();
+    let tantivy_result = get_tantivy_matrix(index.clone(), field).unwrap();
     
-    let max_docid = &tantivy_result.iter().max().unwrap().doc_id;
-    
-    //.iter().next().unwrap().doc_id;
-
-    // Calculate number of documents.
-    // let mut max_docid = 0;
-
-    // for record in &tantivy_result {
-    //     if record.doc_id > max_docid {
-    //         max_docid = record.doc_id;
-    //     }
-    // }
+    // get max docid
+    let max_docid = tantivy_result.iter().max().unwrap().doc_id.clone();
 
     // Map terms (strings) to ids (ints).
     let mut terms_map = HashMap::new();
@@ -65,13 +50,54 @@ fn calculate_lsa(mut tantivy_result: Vec<TantivyDocTermFreq>) -> tantivy::Result
             num_terms = num_terms + 1;
         }
     }
+    println!("RESULT: {:?}", &tantivy_result);
+    let mut ls_matrix = calculate_lsa(tantivy_result, &terms_map, num_terms, max_docid.clone()).unwrap();
+    index.load_searchers();
+    let searcher = index.searcher();
+    let mut total_num_tokens : u64 = 0;
+    for segment_reader in searcher.segment_readers() {
+        let num_tokens = segment_reader.inverted_index(field).total_num_tokens();
+        total_num_tokens = total_num_tokens.clone() + num_tokens;
+    }
 
-    println!("terms map {:?}", terms_map);
-    println!("max_docid {:?}", max_docid);
-    println!("num_terms {:?}", num_terms);
+    let mut new_segment = index.new_segment();
+    println!("NEW SEGMENT: {:?}", new_segment.id());
+    let mut serializer = InvertedIndexSerializer::open(&mut new_segment).expect("Error: create serializer");;
+    let mut field_serializer = serializer.new_field(field.clone(), total_num_tokens).expect("Error: create field_serializer");
+    let rows = ls_matrix.col_iter();
+    //for mut i  in 0..ls_matrix.cols(){
+        //let mut row = rows.next().unwrap();
+        field_serializer.new_term(Term::from_field_text(field, &"hallohuhuwehe").as_slice()).expect("Error: new_term");;
+        field_serializer.write_doc(DocId::from(0 as u32), 10 , &[]).expect("Error: write_doc");;
+        field_serializer.close_term().expect("Error: close term");;
+        field_serializer.close().expect("Error: close field_serializer");       
+    //}
+serializer.close().expect("Error: close serializer");;
+index.load_searchers();
+new_segment.index();
+
+
+println!("{:?}",get_segment_ids(index.clone(), field));    
+println!("{:?}",get_tantivy_matrix(index.clone(), field).unwrap());    
+
+}
+
+fn calculate_lsa(
+    tantivy_result: Vec<TantivyDocTermFreq>, 
+    terms_map: &HashMap<String, usize>, 
+    num_terms: usize,
+    max_docid: u32) -> tantivy::Result<Matrix<f32>> {
+    
+
+
+   
+
+    //println!("terms map {:?}", terms_map);
+    //println!("max_docid {:?}", max_docid);
+    //println!("num_terms {:?}", num_terms);
 
     // Create zeroed matrix with docs as columns and terms as rows.
-    let mut a = Matrix::<f32>::zeros(num_terms, *max_docid as usize + 1);
+    let mut tf_matrix = Matrix::<f32>::zeros(num_terms, max_docid as usize + 1);
 
     // Iterate over result rows.
     for record in &tantivy_result {
@@ -79,57 +105,45 @@ fn calculate_lsa(mut tantivy_result: Vec<TantivyDocTermFreq>) -> tantivy::Result
         println!("record {:?} {:?} {:?}", record.doc_id, *term_index, record.term_freq);
 
         // Set value in matrix.
-        let mut slice = a.sub_slice_mut([*term_index, record.doc_id as usize], 1, 1);
+        let mut slice = tf_matrix.sub_slice_mut([*term_index, record.doc_id as usize], 1, 1);
         let value = slice.iter_mut().next().unwrap();
         *value = record.term_freq as f32;
     }
 
-    print_term_table(&terms_map, &a, "a");
+        print_term_table(&terms_map, &tf_matrix, "A");
+    let lsa_matrix_1 = &tf_matrix * &tf_matrix.transpose();
+    let lsa_matrix_2 = &tf_matrix.transpose() * &tf_matrix;
+    let (_s_square_1, mut u,u_t) = lsa_matrix_1.clone().svd().unwrap();
+    let (_s_square_2, _v, _v_t) = lsa_matrix_2.clone().svd().unwrap();;
+        print_matrix_table(_s_square_1, "s");
+        print_matrix_table(_v.clone(), "v");
+        print_matrix_table(u.clone(), "u");
+        print_matrix_table(u_t.clone(), "u_t");
 
-    let svd = a.clone().svd().unwrap();
-    let (s, mut u, v) = svd;
-    // let (s, v, mut u) = svd;
-    print_matrix_table(s, "s");
-    print_matrix_table(v, "v");
-    print_matrix_table(u.clone(), "u");
-
-     let b = a.transpose();
-    let c = &a * &b;
-    //let c = a.clone();
-    let lu = FullPivLu::decompose(c).unwrap();
+     
+    let lu = FullPivLu::decompose(u.clone()).unwrap();
     let rank = lu.rank();
-    println!("rank {:#?}", rank);
+        println!("rank {:#?}", rank);
 
     // let k = rank - 1;
-    let k = 3;
+    let k = 2;
+    let t_k = u.sub_slice_mut([0, 0], u.rows(), k) * u.sub_slice_mut([0, 0], u.rows(), k).transpose();
+    let tf_matrix_k = &t_k * tf_matrix.clone(); 
+    let meow = &tf_matrix_k - &tf_matrix;
+    print_term_table(&terms_map, &meow, "A-A_k");
+    print_term_table(&terms_map, &tf_matrix, "A");
+    print_term_table(&terms_map, &tf_matrix_k, "A_k");
+    print_term_table(&terms_map, &meow, "T_k");
+    
+    Ok(tf_matrix_k)
 
-    let uk = u.sub_slice_mut([0, 0], u.rows(), k);
-    let uk_transposed = u.sub_slice_mut([0, 0], u.rows(), k).transpose();
-    let t = &uk * &uk_transposed;
-
-    print_matrix_table(t.clone(), "t");
-
-    let ak = &t * a.clone();
-
-    // calculate synonyms?
-    let ak_simplified = ak.clone().apply(&simplify);
-
-    print_term_table(&terms_map, &a, "a");
-    print_term_table(&terms_map, &ak, "ak");
-    print_term_table(&terms_map, &ak_simplified, "ak_simplified");
-
-    // let x = &a - &ak_simplified;
-    // print_term_table(&terms_map, &x, "x");
-
-    Ok(ak)
 }
-
 fn print_matrix_table (matrix: Matrix<f32>, message: &str) {
     let mut table = Table::new();
     for row in matrix.row_iter() {
         let mut fields = vec!();
         for val in row.iter() {
-            let formatted_val = (val * 100.0).round() / 100.0;
+            let formatted_val = val;//(val * 100.0).round() / 100.0;
             fields.push(Cell::new(&formatted_val.to_string()));
         }
         table.add_row(Row::new(fields));
@@ -159,7 +173,7 @@ fn print_term_table (terms_map: &HashMap<String, usize>, matrix: &Matrix<f32>, m
             fields.push(Cell::new(""));
         }
         for val in row.iter() {
-            let formatted_val = (val * 10.0).round();
+            let formatted_val = val.round();//(val * 10.0).round();
             fields.push(Cell::new(&formatted_val.to_string()));
         }
         table.add_row(Row::new(fields));
@@ -178,6 +192,15 @@ struct TantivyDocTermFreq {
     term: String
     // term: Term
 }
+/* #[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
+struct TantivyTermMap {
+    term_map: HashMap<String, usize>,
+    term_freq: u32,
+    // text: &'a [u8]
+    term: String
+    // term: Term
+}
+ */
 
 fn create_test_index<'a>() ->  tantivy::Result<(Index, Field)> {
     // We first create a schema for the sake of the
@@ -188,15 +211,11 @@ fn create_test_index<'a>() ->  tantivy::Result<(Index, Field)> {
     // field. `TEXT` precisely does this.
     let title = schema_builder.add_text_field("title", TEXT | STORED);
     let schema = schema_builder.build();
+    
 
     let index = Index::create_in_ram(schema.clone());
 
     let mut index_writer = index.writer_with_num_threads(1, 50_000_000)?;
-
-    index_writer.add_document(doc!(title => "The Old Man and Mice the Sea"));
-    index_writer.add_document(doc!(title => "Of Mice and Sea the"));
-    index_writer.add_document(doc!(title => "web web web The modern Promotheus Mice"));
-
     index_writer.add_document(doc!(title => "internet web surfing"));
     index_writer.add_document(doc!(title => "internet surfing"));
     index_writer.add_document(doc!(title => "web surfing"));
@@ -204,14 +223,26 @@ fn create_test_index<'a>() ->  tantivy::Result<(Index, Field)> {
     index_writer.add_document(doc!(title => "surfing beach"));
     index_writer.add_document(doc!(title => "surfing beach"));
 
+
     index_writer.commit()?;
     Ok((index, title))
 }
-
-fn get_tantivy_matrix<'a>(index: Index, field: Field) ->  tantivy::Result<Vec<TantivyDocTermFreq>> {
-    index.load_searchers()?;
+fn get_segment_ids(index : Index, field: Field) -> Vec<tantivy::SegmentId> {
+    let mut result = Vec::new();
+    index.load_searchers();
     let searcher = index.searcher();
+    for segment_reader in searcher.segment_readers() {
+        result.push(segment_reader.segment_id());
+        }
+
+result
+}
+
+fn get_tantivy_matrix<'a>(mut index: Index, field: Field) ->  tantivy::Result<Vec<TantivyDocTermFreq>> {
+    
     let mut records = vec!();
+    index.load_searchers();
+    let searcher = index.searcher();
 
     // A tantivy index is actually a collection of segments.
     // Similarly, a searcher just wraps a list `segment_reader`.
@@ -225,7 +256,6 @@ fn get_tantivy_matrix<'a>(index: Index, field: Field) ->  tantivy::Result<Vec<Ta
         // - the term dictionary
         // - the inverted lists associated to each terms and their positions
         let inverted_index = segment_reader.inverted_index(field);
-
 
         // let terms = inverted_index.termdict.
         // println!("termdict {:?}", inverted_index.terms());
